0faa07d7d05064be40dbba9f4059d4dc1b11ce26
ritikbundela <ritikrajbundela004@gmail.com>
2025-11-26 22:33:27 +0530

    Create 136-SingleNumber

---
+++ 136-SingleNumber/136-SingleNumber.cpp
@@ -0,0 +1,11 @@
+// Last updated: 26/11/2025, 22:33:27
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = 0;
+        for(int i=0; i<nums.size();i++){
+            result = result ^ nums[i];
+        }
+        return result;
+    }
+};
