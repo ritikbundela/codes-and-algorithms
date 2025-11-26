a2f97b0d52a5eba52767ce0c016f81dbb523d41e
ritikbundela <ritikrajbundela004@gmail.com>
2025-11-26 22:31:00 +0530

    Create 1388-GreatestSumDivisibleByThree

---
+++ 1388-GreatestSumDivisibleByThree/1388-GreatestSumDivisibleByThree.java
@@ -0,0 +1,20 @@
+// Last updated: 26/11/2025, 22:30:59
+class Solution {
+    public int maxSumDivThree(int[] nums) {
+        int[]dp=new int[3];
+        dp[0]=0;
+        dp[1]=Integer.MIN_VALUE;
+        dp[2]=dp[1];
+
+        for(int val:nums){
+            int[]curr=dp.clone();
+            
+            for(int i=0;i<dp.length;i++){
+                curr[(i+val)%3] = Math.max(curr[(i+val)%3],dp[i]+val);
+            }
+
+            dp=curr;
+        }
+        return dp[0];
+    }
+}
