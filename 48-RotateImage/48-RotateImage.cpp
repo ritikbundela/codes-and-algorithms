74d3519ffdd6a8aa9fefc502d615f5c648d004fc
ritikbundela <ritikrajbundela004@gmail.com>
2025-11-26 22:34:27 +0530

    Create 48-RotateImage

---
+++ 48-RotateImage/48-RotateImage.cpp
@@ -0,0 +1,21 @@
+// Last updated: 26/11/2025, 22:34:27
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+       
+       int n = matrix.size();
+       if(n == 0 || n == 1)
+       return ;
+
+       for(int i=0; i<n; i++){
+        for(int j=i; j<n; j++){
+            swap(matrix[i][j], matrix[j][i]);
+        }
+       }
+
+       for(int i=0; i<n; i++){
+        reverse(matrix[i].begin(), matrix[i].end());
+       }
+
+    }
+};
