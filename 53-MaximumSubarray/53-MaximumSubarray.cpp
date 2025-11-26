0d6ac8eef3a77ad0793332e35ad0a1a3036fd672
ritikbundela <ritikrajbundela004@gmail.com>
2025-11-26 22:34:25 +0530

    Create 53-MaximumSubarray

---
+++ 53-MaximumSubarray/53-MaximumSubarray.cpp
@@ -0,0 +1,15 @@
+// Last updated: 26/11/2025, 22:34:24
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            currentSum = max(nums[i], currentSum + nums[i]);
+            maxSum = max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+};
