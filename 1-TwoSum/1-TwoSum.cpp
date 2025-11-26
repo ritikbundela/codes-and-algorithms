cb8d3ac414933147c0082ba4dce56dfc301cdcbc
ritikbundela <ritikrajbundela004@gmail.com>
2025-11-26 22:35:01 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.cpp
@@ -0,0 +1,15 @@
+// Last updated: 26/11/2025, 22:35:01
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+        for(int i=0; i<nums.size(); i++){
+            int need = target - nums[i];
+            if(mp.find(need) != mp.end()){
+                return {mp[need], i};
+            }
+            mp[nums[i]] = i;
+        }
+        return {-1,-1};
+    }
+};
