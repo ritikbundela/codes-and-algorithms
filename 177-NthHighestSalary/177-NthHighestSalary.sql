fd8864e1097c9ef93e2bb435693820566980d14e
ritikbundela <ritikrajbundela004@gmail.com>
2025-11-26 22:33:02 +0530

    Create 177-NthHighestSalary

---
+++ 177-NthHighestSalary/177-NthHighestSalary.sql
@@ -0,0 +1,13 @@
+-- Last updated: 26/11/2025, 22:33:02
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+DECLARE M INT;
+SET M = N-1;
+  RETURN (
+      # Write your MySQL query statement below.
+      SELECT DISTINCT SALARY
+      FROM EMPLOYEE
+      ORDER BY SALARY DESC
+      LIMIT 1 OFFSET M
+  );
+END
