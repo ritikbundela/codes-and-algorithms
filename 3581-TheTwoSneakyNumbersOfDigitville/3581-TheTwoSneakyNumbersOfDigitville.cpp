82931da1dff5d602df558c455e282f6557f37765
ritikbundela <ritikrajbundela004@gmail.com>
2025-11-26 22:29:53 +0530

    Create 3581-TheTwoSneakyNumbersOfDigitville

---
+++ 3581-TheTwoSneakyNumbersOfDigitville/3581-TheTwoSneakyNumbersOfDigitville.cpp
@@ -0,0 +1,14 @@
+// Last updated: 26/11/2025, 22:29:53
+class Solution {
+public:
+    vector<int> getSneakyNumbers(vector<int>& nums) {
+        int n=nums.size()-2;
+        bitset<100> seen=0;
+        vector<int> ans;
+        for(int x: nums){
+            if (seen[x]) ans.push_back(x);
+            else seen[x]=1;
+        }
+        return ans;
+    }
+};
