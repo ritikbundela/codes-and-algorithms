dd971baa005f618c0c0b8d241fc23f3002d7cd40
ritikbundela <ritikrajbundela004@gmail.com>
2025-11-26 22:31:36 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.cpp
@@ -0,0 +1,17 @@
+// Last updated: 26/11/2025, 22:31:35
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start = 0, end = nums.size();
+        while(start < end){
+            int mid = start + (end - start) / 2;
+            if(nums[mid] == target) 
+            return mid;
+            if(nums[mid] > target)
+            end = mid;
+            else
+            start = mid+1;
+        }
+        return -1;
+    }
+};
